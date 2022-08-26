b95f2ff8620dde2a49b249ec724ff1b9b52476ba
dmitriy.andreev.1995@inbox.ru <dmitriy.andreev.1995@inbox.ru>
2022-08-27 00:54:35 +0300

    All tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+
+Console.Clear();
+Console.WriteLine("введите число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(num);
+if (number[0]==number[2] || number[1]==number[3])
+{
+    Console.WriteLine("число является палиндромом");
+}
+else
+{
+    Console.WriteLine("число не является палиндромом");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+Console.Clear();
+Console.WriteLine("write A");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("write B");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("write A");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("write B");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("write A");
+int i1 = int.Parse(Console.ReadLine());
+Console.WriteLine("write B");
+int i2 = int.Parse(Console.ReadLine());
+
+double ab = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((i1 - i2), 2));
+
+Console.WriteLine(ab); 
